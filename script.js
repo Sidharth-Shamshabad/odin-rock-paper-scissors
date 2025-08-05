477b959609021c68de3608259f8ba14c6ba50fca
Sidharth Shamshabad <sid@techvoe.com>
2025-08-05 15:39:57 -0400

    getHumanChoice complete

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+
+function getHumanChoice() {
+    const humanChoice = prompt('Enter your choice: rock, paper, or scissors');
+    return humanChoice;
+}
